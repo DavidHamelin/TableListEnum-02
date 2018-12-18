ec9fd98ce6374a3b64c9f9d2bde1d749c50d3b78
DavidHamelin <hamelindavid@hotmail.fr>
2018-12-18 13:15:28 +0100

    Ex 02 tableaux Listes Enum

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TabEx02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] week = new string[] { "Lundi", "Mardi", "Mercredi", "Jeudi", "Vendredi", "Samedi", "Dimanche" };
+            
+            {
+                Console.WriteLine(week[1]);
+                Console.WriteLine(week[4]);
+            }
+        }
+    }
+}
